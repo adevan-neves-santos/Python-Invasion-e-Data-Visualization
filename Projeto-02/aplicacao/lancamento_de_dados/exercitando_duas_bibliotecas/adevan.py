1f67abffbcd5b004ee11782e0a0cdd8c42ab79fe
Adevan-Neves <adevannevessantos15@gmail.com>
2022-01-03 11:56:01 -0400

    Finalizando Capítulo sobre Matplotlib e Pygal

--- Projeto-02/aplicacao/lancamento_de_dados/exercitando_duas_bibliotecas/adevan.py
+++ Projeto-02/aplicacao/lancamento_de_dados/exercitando_duas_bibliotecas/adevan.py
@@ -15,10 +15,13 @@ for i in range(100):
     resultados.append(d1.jogar())
 
 frequencias=[]
+possiveis=list(range(1,d1.numero_lados+1))
 for i in range(1,d1.numero_lados+1):
-    f=resultados.count(i)
-    frequencias.append(f)
+    frequencias.append(resultados.count(i))
 
-ladosPossiveis=list(set(resultados)).sort()
-print(len(ladosPossiveis))
-print(len(frequencias))
+plt.plot(possiveis,frequencias,c='green')
+plt.title("Frequências de lados em um dado de 22 com 100 lançamentos")
+plt.xlabel("Lados",fontsize=12)
+plt.ylabel("frequência de lados")
+plt.savefig('aplicacao/lancamento_de_dados/exercitando_duas_bibliotecas/imagens/dadosMatplotlib.png')
+plt.show()
